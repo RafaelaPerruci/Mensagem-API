28e23082aae7f2af4e6729e23bd4be6409511aa7
Rafaela Perruci <rafaelaoperruci@gmail.com>
2023-11-20 08:30:41 -0300

    renomeação do arquivo antigo area-admin

---
+++ jss/scriptLogin.js
@@ -0,0 +1,15 @@
+function fazerRegistro() {
+    const novoEmailUsuario = document.getElementById("novo-email").value;
+    const novaSenhaUsuario = document.getElementById("nova-password").value;
+
+    const dadosRegistro = {email: novoEmailUsuario, senha: novaSenhaUsuario};
+
+    if (validarNovoUsuario(dadosRegistro)) {
+        alert("Registro realizado com sucesso!");
+        window.location.href = "pagina_inicial.html";
+    } else {
+        alert("Erro ao registrar novo usuário");
+    }
+
+    document.getElementById('registro-form').reset();
+}
